aa2d346f423c1abe725a4fa62969cee60b9c191e
g0sttexXx <128875552+g0sttexXx@users.noreply.github.com>
2026-09-03 14:32:18 -0300

    Add README for Jogo da velha.C

---
+++ Jogo da velha.C
@@ -0,0 +1,25 @@
+#include <studio. h>
+
+int main() {
+
+char tabuleiro[3][3] = {
+    {' '.' '.' '.},
+    {' '.' '.' '.},
+    {' '.' '.' '.}
+};
+
+  printf(" %c | %c | %c\n",
+tabuleiro[0][0] tabuleiro[0][0]
+tabuleiro[0][2] );
+  printf("---+---+---\n");
+  printf(" %c | %c | %c\n",
+tabuleiro[1][0] tabuleiro[1][1]
+tabuleiro[1][2] );    
+    printf("---+---+---\n");
+  printf(" %c | %c | %c\n",
+tabuleiro[2][0] tabuleiro[2][2]
+tabuleiro[2][2] );
+
+   return 0;
+
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Jogo-da-velha
-Classic game.
