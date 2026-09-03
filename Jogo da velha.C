d446dd67c023d54e66c2f6ab0251a58250516e12
g0sttexXx <128875552+g0sttexXx@users.noreply.github.com>
2026-09-03 15:44:06 -0300

    Add user input for Tic Tac Toe game

--- Jogo da velha.C
+++ Jogo da velha.C
@@ -8,6 +8,19 @@ char tabuleiro[3][3] = {
     {' '.' '.' '.}
 };
 
+int linha;
+int coluna;
+
+  printf("Escolha uma linha (0, 1 ou 2): ");
+    scanf("%d", &linha);
+
+  printf("Escolha uma coluna (0, 1 ou 2): ");
+    scanf("%d", &coluna);
+
+  tabuleiro [linha][coluna] = 'X';
+
+  printf ("\n");
+
   printf(" %c | %c | %c\n",
 tabuleiro[0][0] tabuleiro[0][0]
 tabuleiro[0][2] );
