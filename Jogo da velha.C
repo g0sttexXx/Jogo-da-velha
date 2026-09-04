9fca48860efc914fabc1acdcc77da8e9789d9fbc
g0sttexXx <128875552+g0sttexXx@users.noreply.github.com>
2026-09-04 15:19:59 -0300

    Implement player switching and fix output formatting
    Add player switching logic and fix print statements

--- Jogo da velha.C
+++ Jogo da velha.C
@@ -10,6 +10,11 @@ char tabuleiro[3][3] = {
 
 int linha;
 int coluna;
+char jogador = 'X';
+
+    while (1) {
+
+  printf(\njogador %c\n, jogador);
 
   printf("Escolha uma linha (0, 1 ou 2): ");
     scanf("%d", &linha);
@@ -22,17 +27,29 @@ int coluna;
   printf ("\n");
 
   printf(" %c | %c | %c\n",
-tabuleiro[0][0] tabuleiro[0][0]
+tabuleiro[0][0]
+tabuleiro[0][0]
 tabuleiro[0][2] );
+
   printf("---+---+---\n");
   printf(" %c | %c | %c\n",
-tabuleiro[1][0] tabuleiro[1][1]
-tabuleiro[1][2] );    
+tabuleiro[1][0] 
+tabuleiro[1][1]
+tabuleiro[1][2] );  
+
     printf("---+---+---\n");
   printf(" %c | %c | %c\n",
-tabuleiro[2][0] tabuleiro[2][2]
+tabuleiro[2][0] 
+tabuleiro[2][2]
 tabuleiro[2][2] );
 
    return 0;
 
+    //Troca jogador
+   if (jogador == 'X') {
+    jogador ='0';
+ } else{ jogador'X';
+}
+    return 0;
+ }
 }
